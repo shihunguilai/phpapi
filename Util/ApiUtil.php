16073fa105350e352b40113498ee86590c01cc9d
cliff <1058462838@qq.com>
2016-09-18 16:38:28 +0800

    add sth.

---
+++ Util/ApiUtil.php
@@ -0,0 +1,27 @@
+<?php
+namespace cliff\Util;
+/**
+ *
+ */
+class ApiUtil
+{
+
+  /**
+   * [myserialize description]
+   * @param  [mixed] $data [description]
+   * @return [string]       [description]
+   */
+  public static function myserialize($data)
+  {
+    return base64_encode(gzcompress(serialize($data)));
+  }
+
+  public static function myunserialize($data)
+  {
+    return unserialize(gzuncompress(base64_decode($data)));
+  }
+
+
+
+
+}
