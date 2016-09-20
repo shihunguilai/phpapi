0ffe15097620557626cf99ad67f4910843c84264
cliff zhang <1058462838@qq.com>
2016-09-20 04:36:08 +0000

    Applied fixes from StyleCI

--- src/Cache/cacheYac.php
+++ src/Cache/cacheYac.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace shihunguilai\phpapi\Cache;
+
 use shihunguilai\phpapi\Util\ApiUtil;
 
 /**
  * Created by PhpStorm.
- * User: clff zhang
+ * User: clff zhang.
  */
-
 class cacheYac
 {
     private $have_extension = false;
@@ -22,7 +23,7 @@ class cacheYac
 
     private function init()
     {
-        if(extension_loaded('yac')){
+        if (extension_loaded('yac')) {
             $this->have_extension = true;
             $this->m = new \Yac($this->cache_pre);
         }
@@ -30,100 +31,118 @@ class cacheYac
 
     public static function getInstance()
     {
-        if(!(self::$instance && self::$instance instanceof self)){
+        if (!(self::$instance && self::$instance instanceof self)) {
             self::$instance = new self();
         }
+
         return self::$instance;
     }
 
     /**
      * @param string $name
-     * @param mixed $value
-     * @param int $ttl
+     * @param mixed  $value
+     * @param int    $ttl
+     *
      * @return bool
      */
-    public function set($name,$value,$ttl = 0)
+    public function set($name, $value, $ttl = 0)
     {
-        if(!$this->have_extension){return false;}
-        $value = ApiUtil::myserialize($value);
-        if(is_int($ttl) && $ttl){
-            return $this->m->set($name,$value,$ttl);
+        if (!$this->have_extension) {
+            return false;
         }
-        return $this->m->set($name,$value);
-    }
+        $value = ApiUtil::myserialize($value);
+        if (is_int($ttl) && $ttl) {
+            return $this->m->set($name, $value, $ttl);
+        }
 
+        return $this->m->set($name, $value);
+    }
 
     /**
      * @param array $kvs
-     * @param int $ttl
+     * @param int   $ttl
+     *
      * @return mixed
      */
-    public function mSet(array $kvs,$ttl = 0)
+    public function mSet(array $kvs, $ttl = 0)
     {
-        if(!$this->have_extension){return false;}
+        if (!$this->have_extension) {
+            return false;
+        }
         // array_walk($kvs,function(&$val){
         //     $val = ApiUtil::myserialize($val);
         // });
-        $kvs = array_map(function($v){
-          return ApiUtil::myserialize($v);
+        $kvs = array_map(function ($v) {
+            return ApiUtil::myserialize($v);
         }, $kvs);
-        if(is_int($ttl) && $ttl){
-            return  $this->m->set($kvs,$ttl);
+        if (is_int($ttl) && $ttl) {
+            return  $this->m->set($kvs, $ttl);
         }
+
         return $this->m->set($kvs);
     }
 
-
     /**
      * @param string | array $name
+     *
      * @return mixed
      */
     public function get($name)
     {
-        if(!$this->have_extension){return null;}
+        if (!$this->have_extension) {
+            return;
+        }
         $tp = $this->m->get($name);
-        if(!$tp || empty($tp)){return $tp;}
-        if(is_string($name)){
+        if (!$tp || empty($tp)) {
+            return $tp;
+        }
+        if (is_string($name)) {
             return ApiUtil::myunserialize($tp);
-        }elseif (is_array($name)) {
+        } elseif (is_array($name)) {
             // array_walk($tp,function(&$val){
             //     $val = ApiUtil::myunserialize($val);
             // });
-            $tp = array_map(function($v){
-              return ApiUtil::myunserialize($v);
+            $tp = array_map(function ($v) {
+                return ApiUtil::myunserialize($v);
             }, $tp);
+
             return $tp;
         }
     }
 
     /**
-     * @param  string | array $name
-     * @param int $delay
+     * @param string | array $name
+     * @param int            $delay
+     *
      * @return mixed
      */
-    public function rm($name,$delay = 0)
+    public function rm($name, $delay = 0)
     {
-        if(!$this->have_extension){return false;}
-        return $this->m->delete($name,$delay);
-    }
+        if (!$this->have_extension) {
+            return false;
+        }
 
+        return $this->m->delete($name, $delay);
+    }
 
     /**
      * @return mixed
      */
     public function clear()
     {
-        if(!$this->have_extension){return false;}
+        if (!$this->have_extension) {
+            return false;
+        }
+
         return $this->m->flush();
     }
 
-
     public function detail()
     {
-        if(!$this->have_extension){return null;}
+        if (!$this->have_extension) {
+            return;
+        }
+
         return $this->m->info();
     }
-
-
-
 }
--- tests/cacheYacTest.php
+++ tests/cacheYacTest.php
@@ -1,66 +1,61 @@
 <?php
+
 namespace shihunguilai\phpapi;
 
 use shihunguilai\phpapi\Cache\cacheYac;
-/**
- *
- */
+
 class cacheYacTest extends \PHPUnit_Framework_TestCase
 {
-
     public function setUp()
     {
-      if(!extension_loaded('yac')){
-        $this->markTestSkipped('no yac extension');
-      }
+        if (!extension_loaded('yac')) {
+            $this->markTestSkipped('no yac extension');
+        }
     }
-
 
     public function tearDown()
     {
-      cacheYac::getInstance()->clear();
+        cacheYac::getInstance()->clear();
     }
 
     public function testYacExt()
     {
-      $this->assertTrue(extension_loaded('yac'));
+        $this->assertTrue(extension_loaded('yac'));
     }
 
-
-    function testGetInstance()
+    public function testGetInstance()
     {
         $a = cacheYac::getInstance();
         $b = cacheYac::getInstance();
-        $this->assertEquals($a,$b);
+        $this->assertEquals($a, $b);
     }
 
-    function testSet()
+    public function testSet()
     {
         $a = 'luck';
         $name = 'aa';
-        cacheYac::getInstance()->set($name,$a);
-        $this->assertEquals($a,cacheYac::getInstance()->get($name));
+        cacheYac::getInstance()->set($name, $a);
+        $this->assertEquals($a, cacheYac::getInstance()->get($name));
     }
 
     public function testmSet()
     {
-      $da = array(
-        "dummy" => "foo",
-        "dummy2" => "foo1",
-      );
+        $da = [
+        'dummy'  => 'foo',
+        'dummy2' => 'foo1',
+      ];
 
-      cacheYac::getInstance()->mSet($da);
+        cacheYac::getInstance()->mSet($da);
 
-      $this->assertEquals(cacheYac::getInstance()->get(array_keys($da)),$da);
-
+        $this->assertEquals(cacheYac::getInstance()->get(array_keys($da)), $da);
     }
 
     public function testRm()
     {
         $name = 'a';
         $value = 'b';
-        cacheYac::getInstance()->set($name,$value);
-        $this->assertEquals(cacheYac::getInstance()->get($name),$value);
+        cacheYac::getInstance()->set($name, $value);
+        $this->assertEquals(cacheYac::getInstance()->get($name), $value);
         cacheYac::getInstance()->rm($name);
         $this->assertFalse(cacheYac::getInstance()->get($name));
     }
@@ -70,18 +65,13 @@ class cacheYacTest extends \PHPUnit_Framework_TestCase
         print_r(cacheYac::getInstance()->detail());
     }
 
-    function testClear()
+    public function testClear()
     {
         $name = 'a';
         $value = 'b';
-        cacheYac::getInstance()->set($name,$value);
+        cacheYac::getInstance()->set($name, $value);
         cacheYac::getInstance()->clear();
         $dd = cacheYac::getInstance()->detail();
-        $this->assertEquals(0,$dd['slots_used']);
+        $this->assertEquals(0, $dd['slots_used']);
     }
-
-
-
-
-
 }
