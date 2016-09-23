df2ff1d936941b45b69437e93de0ec0371154283
cliff <1058462838@qq.com>
2016-09-23 17:16:11 +0800

    fix

--- before-push.sh
+++ before-push.sh
@@ -4,11 +4,5 @@
 ./vendor/bin/php-cs-fixer fix ./src/
 
 composer dump-autoload --optimize
-phpunit
 
-sleep 20
-git add .
-sleep 20
-git commit -m 'push commit'
-sleep 2
-git push
+./vendor/bin/phpunit
