a0167f4d5c4769fa66c61337ad47d153e73a4541
cliff zhang <1058462838@qq.com>
2016-09-29 22:47:23 +0800

    fix

--- public/index.php
+++ public/index.php
@@ -9,6 +9,10 @@ $lf = "<br/>";
 echo Request::getUserAgent().$lf;
 echo Request::getRequestMethod().$lf;
 
+var_dump(Request::is_android());
+var_dump( Request::is_Apple());exit;
+
+
 var_dump(Request::is_ajax());
 
 var_dump(Request::is_get());
