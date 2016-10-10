6e0271688c13fd6f2abe0fa38b5ef34a27edc17f
cliff zhang <1058462838@qq.com>
2016-10-10 21:40:34 +0800

    fix

--- src/Algorithm/MySort.php
+++ src/Algorithm/MySort.php
@@ -4,15 +4,61 @@ namespace shihunguilai\phpapi\Algorithm;
 
 class MySort
 {
+    /**
+     * 快速排序.
+     *
+     * @param array  $arr
+     * @param string $type
+     *                     2016年10月10日-下午9:21:06
+     *
+     * @author  cliff zhang.<1058462838@qq.com>
+     */
+    public static function quick_sort($arr = array(), $type = 'asc')
+    {
+        if (!is_array($arr)) {
+            return false;
+        }
+        $len = count($arr);
+        if ($len <= 1) {
+            return $arr;
+        }
+        $stand = $arr[0];
+        $left_arr = $right_arr = array();
+        for ($i = 1; $i < $len; ++$i) {
+            if (
+                ($type == 'asc' && $arr[$i] <= $stand)
+                ||
+                ($type == 'desc' && $arr[$i] >= $stand)
+               ) {
+                $left_arr[] = $arr[$i];
+            } else {
+                $right_arr[] = $arr[$i];
+            }
+        }
+        $left_arr = self::quick_sort($left_arr, $type);
+        $right_arr = self::quick_sort($right_arr, $type);
+
+        return  array_merge($left_arr, array($stand), $right_arr);
+    }
+
+    /**
+     * 优化的冒泡排序.
+     *
+     * @param array  $arr
+     * @param string $type
+     *                     2016年10月10日-下午9:03:07
+     *
+     * @author  cliff zhang.<1058462838@qq.com>
+     */
     public static function bubble_sort($arr = array(), $type = 'asc')
     {
         if (!is_array($arr)) {
             return false;
         }
-        if (empty($arr)) {
-            return array();
-        }
         $len = count($arr);
+        if ($len <= 1) {
+            return $arr;
+        }
         for ($i = 0; $i < $len; ++$i) {
             $is_sortable = true;
             for ($j = 0; $j < $len - $i - 1; ++$j) {
--- tests/Algorithm/MySortTest.php
+++ tests/Algorithm/MySortTest.php
@@ -10,4 +10,11 @@ class MySortTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(array(245, 87, 9, 7, 5, 4, 3, 3), MySort::bubble_sort($a, 'desc'));
         $this->assertSame(array(3, 3, 4, 5, 7, 9, 87, 245), MySort::bubble_sort($a, 'asc'));
     }
+
+    public function testquick_sort()
+    {
+        $a = array(3, 4, 5, 7, 3, 9, 87, 245);
+        $this->assertSame(array(245, 87, 9, 7, 5, 4, 3, 3), MySort::quick_sort($a, 'desc'));
+        $this->assertSame(array(3, 3, 4, 5, 7, 9, 87, 245), MySort::quick_sort($a, 'asc'));
+    }
 }
